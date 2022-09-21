15c69644b8e501f47c37087d1b7d1581a9546430
unknown <mhdalif12555@gmail.com>
2022-09-21 19:00:24 +0700

    file new

--- cobaaja.java
+++ cobaaja.java
@@ -5,6 +5,6 @@ public class cobaaja {
 
     public static void main(String[] args) {
         System.out.println("halo kak leish");
-        System.out.println("nabila jelek");
+        System.out.println("nabila cantik");
     }
 }
